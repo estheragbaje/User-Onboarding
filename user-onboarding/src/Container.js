b491111557033758e9f7bfb4a37cf549e8487c33
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-19 23:04:42 +0100

    Wrote the validations using yup

--- user-onboarding/src/Container.js
+++ user-onboarding/src/Container.js
@@ -13,3 +13,19 @@ const initialUserForm = {
   password: "",
   terms: false
 };
+
+// Step 2:  Write the validation using Yup
+const userFormValidation = yup.object().shape({
+  name: yup.string().required("You need to supply your name"),
+  age: yup
+    .number()
+    .min(18, "You need to be up to 18 years")
+    .max(60, "You cannot be more than 60 years")
+    .required("You need to put your age"),
+  email: yup
+    .string()
+    .email("Your email is invalid")
+    .required("You need to fill your email"),
+  password: yup.string().required("A password is required"),
+  terms: yup.boolean()
+});
